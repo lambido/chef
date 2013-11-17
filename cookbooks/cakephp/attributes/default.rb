303af5027f240e763b03f322095d1af7c7d45027
Steve Laughlin <lambido@mac.com>
2013-11-16 17:37:08 -0700

    added cake cookbooks

---
+++ cookbooks/cakephp/attributes/default.rb
@@ -0,0 +1,7 @@
+default[:cakephp][:app_dir] = "/mnt/srv/www/strainsource/current/src/app"
+default[:cakephp][:tmp_dir] = default[:cakephp][:app_dir] + "/tmp"
+default[:cakephp][:upload_tmp_dir] = default[:cakephp][:app_dir]  + "/webroot/resources/tmp"
+default[:cakephp][:db_user] = "strainsource"
+default[:cakephp][:db_name] = "strainsource"
+default[:cakephp][:log_level] = "strainsource"
+default[:cakephp][:debug_level] = 0
