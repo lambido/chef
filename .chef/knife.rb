a5fb26720610a54f392d37dfbb2acce94a88617c
Marc Neubauer <marc.neubauer@gmail.com>
2013-12-01 15:52:21 -0700

    moved cakephp into cookbooks added knife.rb config
    Signed-off-by: Marc Neubauer <marc.neubauer@gmail.com>

---
+++ .chef/knife.rb
@@ -0,0 +1,6 @@
+    current_dir = File.dirname(__FILE__)
+    cookbook_path ["#{current_dir}/../cookbooks"]
+	
+	cookbook_copyright "StrainSource"
+    cookbook_email     "tech@strainsource.com"
+    cookbook_license   "apachev2"
